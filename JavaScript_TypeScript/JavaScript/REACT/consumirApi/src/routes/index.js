b3faefe406327cf4d6342c68d85e738b9e8b3ca7
Bruno Sonoda <brunobsonoda@gmail.com>
2026-02-01 12:15:40 -0300

    Update Routes and Pages
    JavaScript_TypeScript\JavaScript\REACT\consumirAPI.
    Adicionando Novas Rotas e Atualizando o Código

--- JavaScript_TypeScript/JavaScript/REACT/consumirApi/src/routes/index.js
+++ JavaScript_TypeScript/JavaScript/REACT/consumirApi/src/routes/index.js
@@ -1,13 +1,51 @@
 import React from "react";
 import { Routes as Switch, Route } from "react-router-dom";
 
+import MyRoute from "./MyRoute";
+
+import Aluno from "../pages/Aluno";
+import Alunos from "../pages/Alunos";
+import Fotos from "../pages/Fotos";
+import Register from "../pages/Register";
 import Login from "../pages/Login";
 import Page404 from "../pages/Page404";
 
 export default function Routes() {
   return (
     <Switch>
-      <Route path="/" element={<Login />} />
+      <Route exact path="/" element={<Alunos />} isClosed={false} />
+
+      <Route
+        path="/aluno/:id/edit"
+        element={
+          <MyRoute isClosed>
+            <Aluno />
+          </MyRoute>
+        }
+      />
+
+      <Route
+        path="/aluno"
+        element={
+          <MyRoute isClosed>
+            <Aluno />
+          </MyRoute>
+        }
+      />
+
+      <Route
+        path="/fotos/:id"
+        element={
+          <MyRoute isClosed>
+            <Fotos />
+          </MyRoute>
+        }
+      />
+
+      <Route exact path="/login" element={<Login />} isClosed={false} />
+
+      <Route exact path="/register" element={<Register />} isClosed={false} />
+
       <Route path="*" element={<Page404 />} />
     </Switch>
   );
